eb778ebf70d7839dec9cb71d4066baf6b71a6e45
ryan.braganza <ryan.braganza@ac703d98-2c6e-11df-8474-0f70896a4c6b>
2010-03-14 11:19:34 +0000

    added basic ajax stuff

--- trunk/controllers/ajax.py
+++ trunk/controllers/ajax.py
@@ -1,4 +1,5 @@
 import cgi
+import logging
 import os
 
 from django.utils import simplejson
@@ -10,20 +11,29 @@ from google.appengine.ext import db
 
 class Ajax(webapp.RequestHandler):
   def get(self):
+    logging.info(self.request.query_string)
+    logging.info(simplejson.dumps(self.request.query_string))
+    for arg in self.request.arguments():
+      logging.info(arg + ': ' + str(self.request.get_all(arg)))
 
     url = None
     url_linktext = None
-    template_values = {
-        'title': 'Welcome',
-        'url': url,
-        'url_linktext': url_linktext,
-        }
+
+    obj = {1:2, 'abcd': True}
 
     path = os.path.join(os.path.dirname(__file__), '../index.html')
-    self.response.out.write(template.render(path, template_values))
+    self.response.out.write(simplejson.dumps(obj))
+
+class Test(webapp.RequestHandler):
+  def get(self):
+    logging.info('in Test')
+    path = os.path.join(os.path.dirname(__file__), '../test.html')
+    self.response.out.write(template.render(path, {}))
 
 application = webapp.WSGIApplication(
-                                     [('/ajax', Ajax)],
+                                     [('/ajax', Ajax),
+                              ('/ajaxtest', Test)
+                                          ],
                                      debug=True)
 
 def main():
